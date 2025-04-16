07056183240731b4cc80913f22013acdfbf56fea
Adarsh Krishna <adarsh240223@gmail.com>
2025-04-16 13:06:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ import About from './Components/About';
 import Intro from './Components/Intro';
 import Projects from './Components/Projects';
 import MyDetails from './Components/Details';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  
   return (
     <div className='App'>
       <div className="wrapper">
